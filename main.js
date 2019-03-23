db23dd06937838fc7acc04d315137ef31b12f722
imoigre <sashalazorenko2001@gmail.com>
2019-03-23 15:12:48 +0300

    Lists the cars

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+function car(name, model, owner, year, phone, image)  {
+  return {
+    name, model, owner, year, phone, image
+  }
+}
+
+const cars = [
+  {name: "Ford", model: "Focus", owner: "Imoigre", year: 2018, phone: "+38 068 316 27 26", image: "images/focus.jpg"},
+  {name: "Ford", model: "Mustang", owner: "Alexey", year: 1968, phone: "+38 063 316 27 26", image: "images/mustang.jpg"},
+  {name: "Ford", model: "Fusion", owner: "Vladimir", year: 2015, phone: "+38 073 316 27 26", image: "images/fusion.jpg"}
+]
+
 new Vue({
- el: ''  
+ el: '#app',
+ data: {
+   cars: cars
+ }
 })
